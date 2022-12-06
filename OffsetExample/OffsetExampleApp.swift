fbe1dee3c6ae3bd945e1bcfb28a11e0dccdcc8ee
Edgard <20133252@ealoe.ulima.edu.pe>
2022-12-06 18:04:14 -0500

    Initial Commit

---
+++ OffsetExample/OffsetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffsetExampleApp.swift
+//  OffsetExample
+//
+//  Created by EdgardVS on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OffsetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
